181067ad34df542ea82b42e901cc3b6fe2b1af27
Kinjal <kkanjiya849@rku.ac.in>
2023-03-02 12:45:05 +0530

    JSX Code added

---
+++ Day 001 JSX/src/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Hello from './Hello'
+import Food from './MyFavouriteFood'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <Hello></Hello>
+)
