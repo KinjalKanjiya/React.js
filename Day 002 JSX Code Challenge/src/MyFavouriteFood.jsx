f00a1773f64c7c7b90b70db02824ce71c48ddc35
Kinjal <kkanjiya849@rku.ac.in>
2023-03-11 21:34:57 +0530

    Jsx Code Challange

---
+++ Day 002 JSX Code Challenge/src/MyFavouriteFood.jsx
@@ -0,0 +1,22 @@
+//Challange Design this.
+//Create a react app from scratch . 
+//It Should display a h1 heading.
+//It should display an unordered list
+//li should contain 3 list elements.
+
+
+import React from "react";
+function Food(){
+    return(
+        <div>
+            <h1>My Favourite Food</h1>
+            <ul>
+                <li>Banana</li>
+                <li>Apple</li>
+                <li>kiwi</li>
+            </ul>
+        </div>
+    )
+
+}
+export default Food;
---
+++ Day 002 JSX Code Challenge/src/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Food from './MyFavouriteFood'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <Food></Food>
+  
+)
